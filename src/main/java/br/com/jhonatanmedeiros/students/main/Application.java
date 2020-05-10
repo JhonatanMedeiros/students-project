e6f85d4dbbb5f5d2499d52f30dc82057c6de6511
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2020-05-10 19:41:14 -0300

    first commit

---
+++ src/main/java/br/com/jhonatanmedeiros/students/main/Application.java
@@ -0,0 +1,16 @@
+package br.com.jhonatanmedeiros.students.main;
+
+/**
+ *
+ * @author Jhonatan Hardt de Medeiros<https://jhonatanmedeiros.com>
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
